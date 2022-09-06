39121c2af51d7ba68460a82cdb4c0e1c2914e7bf
Vanessa Farias <vr.farias1992@gmail.com>
2022-09-06 15:57:19 -0300

    exercício Condicionais I JS

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt("Qua a sua idade?")
+let ensino = prompt("Fez o ensino médio?")
+let faculdade = prompt("Faz outra faculdade?")
 
-if(){
-
+if(idade >= 18){
+    console.log("você é maior de idade")
 }else{
-	
-}
+	console.log("você é menor de idade")
+};
+
+if(ensino === "sim"){
+    console.log("você fez o ensino médio")
+}else{
+    console.log("você não fez o ensino médio")
+};
+
+if(faculdade === "sim"){
+    console.log("você faz outra faculdade")
+}else{
+    console.log("você não faz outra faculdade")
+};
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,22 @@
-let nacionalidade //= adicione o prompt aqui
+let qualNac = prompt("Qual a sua nacionalidade?").toLowerCase();
+let nacionalidade = {
+    nac1: "brasileira",
+    nac2: "argentina",
+    nac3: "uruguaia",
+    nac4: "chilena",
+    nac5: "colombiana"
+};
+
+if(nacionalidade.nac1 === qualNac){
+    console.log("Encontrada")
+}else if(nacionalidade.nac2 === qualNac){
+    console.log("Encontrada")
+}else if(nacionalidade.nac3 === qualNac){
+    console.log("Encontrada")
+}else if(nacionalidade.nac4 === qualNac){
+    console.log("Encontrada")
+}else if(nacionalidade.nac5 === qualNac){
+    console.log("Encontrada")
+}else{
+    console.log("Não encontrada")
+};
